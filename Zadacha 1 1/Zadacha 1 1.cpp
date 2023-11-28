7a78af941fc35b03036a32e19023f121f56c386a
User <User@WIN-A8PT6RTL6ET>
2023-11-28 19:17:03 +0200

    Добавьте файлы проекта.

---
+++ Zadacha 1 1/Zadacha 1 1.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <limits>
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Size of int: " << sizeof(int) << " bytes ";
+
+    std::cout << "Minimum value of int: " << std::numeric_limits<int>::lowest();
+      
+    std::cout << "Maximum value of int: " << std::numeric_limits<int>::max();
+
+        return 0;
+}
+
+
+
